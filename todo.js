defc1532e04b3c7c2861a6ca665df3a34075229f
Zan <zanasmakarov@gmail.com>
2020-07-14 18:12:55 +0100

    added input, updated on click

--- todo.js
+++ todo.js
@@ -1,12 +1,22 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li" , function(){
     $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span" , function(event){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     event.stopPropogation();
 })
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grab new todo text from input
+        var todoText = $(this).val()
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append(`<li><span>X</span> ${todoText}</li>`)
+    }
+});
